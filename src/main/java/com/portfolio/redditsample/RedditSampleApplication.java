03c654cb017dc936e7a059752f82c169c3b92ca0
Jose Meono <jose.a.meono@gmail.com>
2019-10-30 19:34:30 -0500

    first commit

---
+++ src/main/java/com/portfolio/redditsample/RedditSampleApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.redditsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/redditsample/RedditSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.redditsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
