d02ef71968ac91074d91d9238e19c2d56f074d3e
Jose Meono <jose.a.meono@gmail.com>
2019-12-10 22:44:04 -0600

    WIP: TDD With MVC

--- src/main/java/com/portfolio/redditsample/controller/UserController.java
+++ src/main/java/com/portfolio/redditsample/controller/UserController.java
@@ -1,8 +1,26 @@
 package com.portfolio.redditsample.controller;
 
+import com.portfolio.redditsample.model.User;
+import com.portfolio.redditsample.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
+    //Sign Up:
+    @PostMapping(value = "/create-user", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
+    }
 }
