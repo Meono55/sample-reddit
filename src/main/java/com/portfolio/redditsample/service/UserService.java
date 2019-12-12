ce724620bc6c019d6b43b338a8fc233fa040775c
Jose Meono <jose.a.meono@gmail.com>
2019-12-11 19:54:50 -0600

    WIP: working on basic controllers for users

--- src/main/java/com/portfolio/redditsample/service/UserService.java
+++ src/main/java/com/portfolio/redditsample/service/UserService.java
@@ -5,6 +5,8 @@ import com.portfolio.redditsample.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -14,5 +16,14 @@ public class UserService {
     public User createUser(User user) {
         User createdUser = userRepo.save(user);
         return createdUser;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+
+    public User getUserById(Long id) {
+
+        return userRepo.findById(id);
     }
 }
